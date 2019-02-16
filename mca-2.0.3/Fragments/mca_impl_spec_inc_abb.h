f590b084d435858b07e0b3dcb85210c07dd76f95
K. Eric Harper <eric.e.harper@us.abb.com>
2019-02-16 18:06:33 -0500

    Baseline Linux build

--- mca-2.0.3/Fragments/mca_impl_spec_inc_abb.h
+++ mca-2.0.3/Fragments/mca_impl_spec_inc_abb.h
@@ -1,5 +1,9 @@
 #if (__unix__||__MINGW32__)
+#include <signal.h>
 #include <pthread.h>
+#include <sys/types.h>
+#include <bits/types/sigset_t.h>
+#include <bits/sigaction.h>
 #else
 #endif  /* !(__unix__||__MINGW32__ */
 #include <assert.h>
