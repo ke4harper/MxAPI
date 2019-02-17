3db93e747a60dc00841c9a60ef15df0cb93ed34d
K. Eric Harper <eric.e.harper@us.abb.com>
2019-02-16 19:13:06 -0500

    Baseline Linux build

--- mca-2.0.3/Fragments/mca_impl_spec_inc.h
+++ mca-2.0.3/Fragments/mca_impl_spec_inc.h
@@ -1,9 +1,8 @@
 #if (__unix__||__MINGW32__)
+#include <time.h>
+#include <bits/types/clockid_t.h>
 #include <signal.h>
 #include <pthread.h>
-#include <sys/types.h>
-#include <bits/types/sigset_t.h>
-#include <bits/sigaction.h>
 #endif  /* (__unix__||__MINGW32__ */
 #include <assert.h>
 #include <stdarg.h>
