fd203b08d105dfd43eadaa572279d1dfcb40d281
ke4harper <keharper@wt.net>
2022-09-17 18:43:32 -0400

    Global debug level
    Thread safe programmatic control of diagnostic messages

--- mca-rust/common/src/lib.rs
+++ mca-rust/common/src/lib.rs
@@ -1,5 +1,43 @@
 //! Common functions
 
+use std::sync::atomic::{AtomicUsize, Ordering};
+
+// Global debug setting
+static MCA_DEBUG: AtomicUsize = AtomicUsize::new(0);
+static MCA_DEBUG_INITIALIZED: AtomicUsize = AtomicUsize::new(0);
+
+/// Get global debug level
+#[macro_export]
+macro_rules! mca_debug {
+    // Accessor
+    () => {{
+	{
+	    use std::env;
+	    
+	    let initialized = MCA_DEBUG_INITIALIZED.load(Ordering::Relaxed);
+	    if initialized <= 0 {
+		let key = "MCA_DEBUG";
+		match env::var(key) {
+		    Ok(val) => MCA_DEBUG.store(val.parse::<usize>().expect("$MCA_DEBUG is not a number"), Ordering::Relaxed),
+		    Err(_) => MCA_DEBUG.store(0, Ordering::Relaxed),
+		}
+		MCA_DEBUG_INITIALIZED.store(1, Ordering::Relaxed);
+	    }
+
+	    let level = MCA_DEBUG.load(Ordering::Relaxed);
+	    
+	    level
+	}
+    }};
+    // Mutator
+    ($level: expr) => {{
+	{
+	    MCA_DEBUG.store($level, Ordering::Relaxed);
+	    MCA_DEBUG_INITIALIZED.store(1, Ordering::Relaxed);
+	}
+    }};
+}
+
 /// mca_dformat! - diagnostic formatted message with variable args
 /// Formatting string is template for args
 #[macro_export]
@@ -27,13 +65,10 @@ macro_rules! mca_dprintf {
     // No format and variable arguments
     ($level: expr) => {{
 	{
-	    use std::env;
 	    use std::process;
 	    use std::thread;
 	    
-	    let debug_str = env!("MCA_DEBUG", "$MCA_DEBUG is not set");
-	    let debug = debug_str.parse::<u32>().expect("$MCA_DEBUG not a number");
-	    if $level as u32 <= debug {
+	    if $level as usize <= mca_debug!() {
 		print!("/* MCA PID:{} {:?} */   //", process::id(), thread::current().id());
 	    }
 	}
@@ -49,6 +84,11 @@ macro_rules! mca_dprintf {
 	mca_dformat! { $fmt, $($args), * }
     }};
 }    
+
+/// mca_set_debug_level - programmically control diagnostic reporting level
+fn mca_set_debug_level(level: usize) {
+    mca_debug!(level);
+}
 
 #[cfg(test)]
 mod tests { 
@@ -67,5 +107,11 @@ mod tests {
     #[test]
     fn print_args() {
 	mca_dprintf!(0, "{} {}", "This is an arg", 2);
+    }
+
+    #[test]
+    fn set_debug_level() {
+	mca_set_debug_level(1);
+	assert_eq!(1, mca_debug!());
     }
 }
