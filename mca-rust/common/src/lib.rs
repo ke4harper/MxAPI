58981d2a68dd5dad608a2a30f32fb06785056518
ke4harper <keharper@wt.net>
2022-09-18 07:27:45 -0400

    crc32
    Placeholder for crc [3.0.0]

--- mca-rust/common/src/lib.rs
+++ mca-rust/common/src/lib.rs
@@ -1,10 +1,38 @@
-//! Common functions
+///
+/// Copyright(c) 2022, Karl Eric Harper
+/// All rights reserved.
+///
+/// Redistribution and use in source and binary forms, with or without
+/// modification, are permitted provided that the following conditions are met :
+///
+/// * Redistributions of source code must retain the above copyright
+///   notice, this list of conditions and the following disclaimer.
+/// * Redistributions in binary form must reproduce the above copyright
+///   notice, this list of conditions and the following disclaimer in the
+///   documentation and/or other materials provided with the distribution.
+/// * Neither the name of Karl Eric Harper nor the names of its contributors may be used
+///   to endorse or promote products derived from this software without specific
+///   prior written permission.
+///
+/// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+/// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+/// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+/// DISCLAIMED.IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+/// DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+/// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+/// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+/// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+/// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+/// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+///
+
+/// Common functions
 
 use std::sync::atomic::{AtomicUsize, Ordering};
 
 // Global debug setting
-static MCA_DEBUG: AtomicUsize = AtomicUsize::new(0);
-static MCA_DEBUG_INITIALIZED: AtomicUsize = AtomicUsize::new(0);
+static _MCA_DEBUG: AtomicUsize = AtomicUsize::new(0);
+static _MCA_DEBUG_INITIALIZED: AtomicUsize = AtomicUsize::new(0);
 
 /// Get global debug level
 #[macro_export]
@@ -14,17 +42,17 @@ macro_rules! mca_debug {
 	{
 	    use std::env;
 	    
-	    let initialized = MCA_DEBUG_INITIALIZED.load(Ordering::Relaxed);
+	    let initialized = _MCA_DEBUG_INITIALIZED.load(Ordering::Relaxed);
 	    if initialized <= 0 {
 		let key = "MCA_DEBUG";
 		match env::var(key) {
-		    Ok(val) => MCA_DEBUG.store(val.parse::<usize>().expect("$MCA_DEBUG is not a number"), Ordering::Relaxed),
-		    Err(_) => MCA_DEBUG.store(0, Ordering::Relaxed),
+		    Ok(val) => _MCA_DEBUG.store(val.parse::<usize>().expect("$MCA_DEBUG is not a number"), Ordering::Relaxed),
+		    Err(_) => _MCA_DEBUG.store(0, Ordering::Relaxed),
 		}
-		MCA_DEBUG_INITIALIZED.store(1, Ordering::Relaxed);
+		_MCA_DEBUG_INITIALIZED.store(1, Ordering::Relaxed);
 	    }
 
-	    let level = MCA_DEBUG.load(Ordering::Relaxed);
+	    let level = _MCA_DEBUG.load(Ordering::Relaxed);
 	    
 	    level
 	}
@@ -32,8 +60,8 @@ macro_rules! mca_debug {
     // Mutator
     ($level: expr) => {{
 	{
-	    MCA_DEBUG.store($level, Ordering::Relaxed);
-	    MCA_DEBUG_INITIALIZED.store(1, Ordering::Relaxed);
+	    _MCA_DEBUG.store($level, Ordering::Relaxed);
+	    _MCA_DEBUG_INITIALIZED.store(1, Ordering::Relaxed);
 	}
     }};
 }
@@ -83,11 +111,31 @@ macro_rules! mca_dprintf {
 	mca_dprintf! { $level }
 	mca_dformat! { $fmt, $($args), * }
     }};
-}    
+}
 
-/// mca_set_debug_level - programmically control diagnostic reporting level
+/// mca_set_debug_level - programmically control diagnostic reporting verbosity
 fn mca_set_debug_level(level: usize) {
     mca_debug!(level);
+}
+
+/// crc32_compute_buf - computes the CRC-32 value of a memory buffer
+//
+//       Computes or accumulates the CRC-32 value for a memory buffer.
+//       The in_crc32 gives a previously accumulated CRC-32 value to allow
+//       a CRC to be generated for multiple sequential buffer-fuls of data.
+//       The in_crc32 for the first buffer must be zero.
+//
+//  Parameters:
+//       in_crc32 - accumulated CRC-32 value, must be 0 on first call
+//       buf     - buffer to compute CRC-32 value for
+//       buf_len  - number of bytes in buffer
+//
+//  Returns: crc32 - computed CRC-32 value
+//
+fn crc32_compute_buf(_in_crc32: u32, _buf: &u8, _buf_len: usize) -> u32 {
+    println!("/// ERROR: replace crc32_compute_buf with native crc [3.0.0]");
+
+    0
 }
 
 #[cfg(test)]
