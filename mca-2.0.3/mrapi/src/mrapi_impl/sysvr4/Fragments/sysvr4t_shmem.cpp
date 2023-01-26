98aa900c4f199c6daf04679810e61d51358576e4
ke4harper <ke4harper@users.noreply.github.com>
2023-01-26 16:46:38 -0500

    Update sysvr4t_shmem.cpp
    Pretty print

--- mca-2.0.3/mrapi/src/mrapi_impl/sysvr4/Fragments/sysvr4t_shmem.cpp
+++ mca-2.0.3/mrapi/src/mrapi_impl/sysvr4/Fragments/sysvr4t_shmem.cpp
@@ -28,64 +28,64 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 // Get, create, duplicate shared memory
 	{
-		uint32_t id1 = 0;
-		uint32_t id2 = 0;
-		uint32_t id3 = 0;
-		int key = 0;
-        int created = 0;
-        int proc = 0;
-		assert(sys_file_key(NULL,'h',&key));
-        created = 0;
-		if((uint32_t)-1 == (id1 = sys_shmem_get(key,10))) { // race with other process?
+	  uint32_t id1 = 0;
+	  uint32_t id2 = 0;
+	  uint32_t id3 = 0;
+	  int key = 0;
+	  int created = 0;
+	  int proc = 0;
+	  assert(sys_file_key(NULL,'h',&key));
+	  created = 0;
+	  if((uint32_t)-1 == (id1 = sys_shmem_get(key,10))) { // race with other process?
             created = 1;
             id1 = sys_shmem_create(key,10);
-		}
-		assert(0 < id1);
-		id2 = sys_shmem_get(key,10);
-		assert(0 < id2);
+	  }
+	  assert(0 < id1);
+	  id2 = sys_shmem_get(key,10);
+	  assert(0 < id2);
 #if !(__unix__)
-		assert(id1 != id2);
+	  assert(id1 != id2);
 #else
-		assert(id1 == id2);
+	  assert(id1 == id2);
 #endif  // (__unix__)
 #if !(__unix__)
-        proc = (int)GetProcessId(GetCurrentProcess());
+	  proc = (int)GetProcessId(GetCurrentProcess());
 #endif  // !(__unix__)
-		assert(sys_shmem_duplicate(id2,proc,&id3));
-		assert(0 < id3);
+	  assert(sys_shmem_duplicate(id2,proc,&id3));
+	  assert(0 < id3);
 #if !(__unix__)
-		assert(id2 != id3);
+	  assert(id2 != id3);
 #else
-		assert(id2 == id3);
+	  assert(id2 == id3);
 #endif  // (__unix__)
-        if(created) {
+	  if(created) {
             assert(sys_shmem_delete(id1));
-        }
-        else {
+	  }
+	  else {
             assert(sys_shmem_release(id1)); 
-        }
-		assert(sys_shmem_release(id2));
-		assert(sys_shmem_release(id3));
+	  }
+	  assert(sys_shmem_release(id2));
+	  assert(sys_shmem_release(id3));
 	}
 
-	// Attach, detach shared memory
+// Attach, detach shared memory
 	{
-		uint32_t id = 0;
-		void* addr1 = NULL;
-		void* addr2 = NULL;
-		int key = 0;
-		assert(sys_file_key(NULL,'i',&key));
-		if((uint32_t)-1 == (id = sys_shmem_get(key,10))) { // race with other process?
+	  uint32_t id = 0;
+	  void* addr1 = NULL;
+	  void* addr2 = NULL;
+	  int key = 0;
+	  assert(sys_file_key(NULL,'i',&key));
+	  if((uint32_t)-1 == (id = sys_shmem_get(key,10))) { // race with other process?
             id = sys_shmem_create(key,10);
-		}
-		assert(0 < id);
-		addr1 = sys_shmem_attach(id);
-		assert(NULL != addr1);
-		addr2 = sys_shmem_attach(id);
-		assert(NULL != addr2);
-		assert(addr1 != addr2);
-		assert(sys_shmem_detach(addr1));
-		assert(sys_shmem_detach(addr2));
-        assert(sys_shmem_delete(id)); // clean up Windows resources,
-                                      // possible Unix error is ignored
+	  }
+	  assert(0 < id);
+	  addr1 = sys_shmem_attach(id);
+	  assert(NULL != addr1);
+	  addr2 = sys_shmem_attach(id);
+	  assert(NULL != addr2);
+	  assert(addr1 != addr2);
+	  assert(sys_shmem_detach(addr1));
+	  assert(sys_shmem_detach(addr2));
+	  assert(sys_shmem_delete(id)); // clean up Windows resources,
+	  // possible Unix error is ignored
 	}
